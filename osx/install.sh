531a9deadc2f7caf636255c487e7207057a9d51e
changj <justin.chang@agilysys.com>
2018-06-14 09:48:46 -0400

    Create osx script

---
+++ osx/install.sh
@@ -0,0 +1,10 @@
+# Set sidebar icon size to small
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+# Set an insanely fast keyboard repeat rate
+defaults write NSGlobalDomain InitialKeyRepeat -int 18
+defaults write NSGlobalDomain KeyRepeat -int 3
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
